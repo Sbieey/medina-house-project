1baf7cac2e1f0bb1c145453109c20feb01128e8b
Saad Naeem <jsaad3663@gmail.com>
2024-07-06 18:01:54 +0500

    Fix: submit button in contact us, button in home page and footer

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <div
         id="HOME"
-        className=" flex pb-5 justify-center w-auto lg:w-[99%]"
+        className="flex pb-5 justify-center w-auto lg:w-[99%]"
         style={{ background: `url(${img})` }}
       >
         <div className="bg-[#019147] mt-100 w-[95%] h-[706px] relative top-10 rounded-2xl">
@@ -17,25 +17,25 @@ const Home = () => {
             </span>
           </div>
           <div className="flex justify-center">
-            <text className="text-white text-center font-semibold text-3xl sm:text-4xl md:text-4xl lg:text-6xl w-[100%] lg:relative  mt-9">
+            <text className="text-white text-center font-semibold text-3xl sm:text-4xl md:text-4xl lg:text-6xl w-[100%] lg:relative mt-9">
               Madina House CRM -<br /> Empowering Muslim Community
             </text>
           </div>
           <div className="flex justify-center">
-            <p className="text-center relative  justify-items-center text-white mt-5 lg:text-2xl w-[70%]">
+            <p className="text-center relative justify-items-center text-white mt-5 lg:text-2xl w-[70%]">
               Strengthening muslim communities through one platform. We invite
               you to join us in this digital journey to strengthen our community
               and foster spiritual growth
             </p>
           </div>
-          <div className="flex flex-row justify-center relative top-40 ">
+          <div className="flex flex-col items-center sm:flex-row gap-3 px-2 justify-center  relative top-32 sm:top-40">
             <button
               type="button"
-              className="animate-pulse hover:animate-none shadow-2xl shadow-[#D3C00D] text-white bg-[#D3C00D] hover:bg-[#D3C00D] focus:ring-4 focus:outline-none font-medium rounded-lg text-xs sm:text-sm px-1.5 sm:px-5 py-0 sm:py-2.5 text-center inline-flex items-center dark:hover:bg-[#D3C00D] dark:focus:ring-[#D3C00D]"
+              className="flex justify-center items-center animate-pulse hover:animate-none shadow-2xl shadow-[#D3C00D] text-white bg-[#D3C00D] hover:bg-[#D3C00D] focus:ring-4 focus:outline-none font-medium rounded-lg  text-xs sm:text-sm px-6 py-3 w-[270px] sm:w-auto sm:px-12 sm:py-3.5"
             >
-              Start A Free Trail
+              Start A Free Trial
               <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                className="rtl:rotate-180 w-4 h-4 ml-2"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -43,9 +43,9 @@ const Home = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg>
@@ -53,7 +53,7 @@ const Home = () => {
 
             <button
               type="button"
-              className="mt-3  animate-bounce lg:relative left-6 shadow-[#D3C00D] text-[#D3C00D] border border-yellow-400 hover:text-[#fefff5] hover:border-[#dbeb63] focus:ring-4 focus:outline-none font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#D3C00D]"
+              className="flex justify-center items-center animate-bounce shadow-2xl shadow-[#D3C00D] text-[#D3C00D] border border-[#D3C00D] bg-transparent hover:bg-[#D3C00D] hover:text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-xs sm:text-sm px-6 py-3 mt-2 sm:mt-0 w-[270px] sm:w-auto sm:px-8 sm:py-3.5"
             >
               Learn About Madina House
             </button>
